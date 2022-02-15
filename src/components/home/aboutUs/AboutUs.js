9b7d622cbcd76da466d9f0028e0b2a6009329a7c
okoa kinsey <okoakinsey@okoas-MacBook-Pro.local>
2022-02-15 10:55:49 -0500

    fixing homepage slider

--- src/components/home/aboutUs/AboutUs.js
+++ src/components/home/aboutUs/AboutUs.js
@@ -3,13 +3,13 @@ import { Carousel } from "react-bootstrap";
 const text_array = [
   {
     id: "who_we_are",
-    class: "slide active",
+    class: "about_us_slide active",
     title: "who we are",
     text: "Apex is a boutique Agile consultancy with decades of software development experience. We grew up in the era of structured design methodologies, adapted our learning to the rapid development approaches, and graduated to Agile. Wherever you are in your softwareevolution, we are there to help in your journey.",
   },
   {
     id: "what_we_do",
-    class: "slide",
+    class: "about_us_slide",
     title: "what we do",
     text: "Text 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   },
@@ -19,39 +19,42 @@ const text_array = [
   // },
 ];
 
-const about_us_slides = text_array.map((slide, index) => {
+const about_us_slides = text_array.map((about_us_slide, index) => {
   return (
     <div
-      id={`slide_${slide.id}`}
-      className={slide.class}
+      id={`about_us_slide_${about_us_slide.id}`}
+      className={about_us_slide.class}
       style={{ width: "500px" }}
     >
-      <h2 style={{ "text-transform": "capitalize" }}>{slide.title}</h2>
-      {slide.text}
+      <h2 style={{ "text-transform": "capitalize" }}>{about_us_slide.title}</h2>
+      {about_us_slide.text}
     </div>
   );
 });
 
-const setPrev = (slide) => (slide.className = "slide prev");
+const setPrev = (about_us_slide) =>
+  (about_us_slide.className = "about_us_slide prev");
 
 const resetSlides = (slides) => {
   return slides.map((s) => {
-    return (s.className = "slide");
+    return (s.className = "about_us_slide");
   });
 };
 
 const animateSlider = (e) => {
   const id = e.target.id;
-  const currentSlide = document.getElementById(id.replace("button", "slide"));
+  const currentSlide = document.getElementById(
+    id.replace("button", "about_us_slide")
+  );
 
-  const prevSlide = document.querySelector(".about_us .slide.active");
+  const prevSlide = document.querySelector(".about_us .about_us_slide.active");
 
-  if (id === prevSlide.id.replace("slide", "button")) {
-    console.log("current slide exiting...");
+  if (id === prevSlide.id.replace("about_us_slide", "button")) {
+    console.log("current about_us_slide exiting...");
     return;
   }
   setPrev(prevSlide);
-  currentSlide.className = "slide active";
+  currentSlide.className = "about_us_slide active";
   setTimeout(() => resetSlides([prevSlide]), 750);
 };
 
@@ -83,7 +86,7 @@ const AboutUs = () => {
         </div>
         <div id="about_us_text">
           <div className="view-window">
-            <div className="slider">{about_us_slides}</div>
+            <div className="about_us_slider">{about_us_slides}</div>
           </div>
         </div>
       </div>
@@ -102,7 +105,21 @@ const AboutUsStyles = () => {
     #about_us_text{
       text-align:left;
     }
-    #about_us .slide {
+    .about_us .about_us_slide {
+      width: 500px;
+    }
+    .about_us .about_us_slider {
+      width: 500px;
+      position: relative;
+      display: flex;
+      height: 178px;
+    }
+    .about_us .view-window {
+      position: relative;
+      overflow: hidden;
+      height: 178px;
+    }
+    #about_us .about_us_slide {
       display: block;
       position: absolute;
       right: -500px;
@@ -110,12 +127,12 @@ const AboutUsStyles = () => {
       background: #efefef;
       min-height: 180px;
     }
-    #about_us .slide.active{
+    #about_us .about_us_slide.active{
       display:block;
       right:0;
       z-index:2
     }
-    #about_us .slide.prev{
+    #about_us .about_us_slide.prev{
       z-index:1;
       display:block;
       right:500px;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import PublicAssessment from "./components/public_assessment/publicAssessment";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { render } from "react-dom";
+
+import reportWebVitals from "./reportWebVitals";
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
