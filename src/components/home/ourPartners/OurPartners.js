02dd6b0367628e70cf5bcb73bd4f3af41bfd32d5
okoa kinsey <okoakinsey@dhcp-wifi-8021x-155-41-54-162.bu.edu>
2022-02-18 16:59:15 -0500

    updating links on our partners section

--- src/components/home/ourPartners/OurPartners.js
+++ src/components/home/ourPartners/OurPartners.js
@@ -4,20 +4,17 @@ const OurPartners = () => {
       <h1>Our Partners</h1>
       <div className="main flex">
         <div>
-          <h2>Scrum Alliance</h2>
           <a href="https://www.scrumalliance.org/">
             <img src="images/scrum_alliance.png" alt="partner logo" />
           </a>
         </div>
         <div>
-          <h2>SAFE</h2>
-          <a href="https://www.scaledagileframework.com/">
+          <a href="https://www.scrum.org">
             <img src="images/scrum-org-logo.png" alt="partner logo" />
           </a>
         </div>
         <div>
-          <h2>Scrum.org</h2>
-          <a href="https://www.scrum.org">
+          <a href="https://www.scaledagileframework.com/">
             <img src="images/safe-logo.png" alt="partner logo" />
           </a>
         </div>
