21888442e9cd805cc9984fde418bffa272310cf5
okoa kinsey <okoakinsey@dhcp-wifi-8021x-155-41-54-162.bu.edu>
2022-02-18 16:32:24 -0500

    updating footer to match wire frame

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,42 +9,54 @@ const Header = () => {
   return (
     <>
       <section id="footer" style={{ background: "light-gray" }}>
+        {/* <div className="main flex"> */}
         <Link to="/" id="logo">
           <img src="images/logo.png" alt="Edura Logo" />
         </Link>
+        <div>
+          <h3>Related Links</h3>
+          <ul>
+            <li>
+              <Link to="contact_us">About Us</Link>
+            </li>
+            <li>
+              <Link to="contact_us"> Contact Us</Link>
+            </li>
+            <li>
+              <Link to="contact_us"> Become A Trainer</Link>
+            </li>
+            <li>
+              <Link to="contact_us">Terms Conditions</Link>
+            </li>
+          </ul>
+        </div>
 
-        <ul>
-          <li>
-            <Link to="/">sign up</Link>
-          </li>
-          <li>|</li>
-          <li>
-            <Link to="/">sign in</Link>
-          </li>
-        </ul>
-        <ul className="social_media_links">
-          <li>
-            <Link to="/">
-              <Twitter />
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <Youtube />
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <Linkedin />
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <Facebook />
-            </Link>
-          </li>
-        </ul>
-        <Outlet />
+        <div>
+          <h3 style={{ color: "white" }}>Follow Us</h3>
+          <ul className="social_media_links">
+            <li>
+              <Link to="/">
+                <Twitter />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <Youtube />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <Linkedin />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <Facebook />
+              </Link>
+            </li>
+          </ul>
+        </div>
+        {/* </div> */}
       </section>
     </>
   );
