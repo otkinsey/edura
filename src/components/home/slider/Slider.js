c0763c56b2588a2ad94e307ae63d43667320a6a2
okoa kinsey <okoakinsey@okoas-MacBook-Pro.local>
2022-02-19 09:38:38 -0500

    removing coaching section from homepage

--- src/components/home/slider/Slider.js
+++ src/components/home/slider/Slider.js
@@ -26,12 +26,12 @@ const Slider = () => {
       text: "Apex coursework will thoroughly prepare you for Agile certification. Having worked side-byside with the three elite certification agencies, we know just what you will need to gain certification from the three important certification organizations <a href='https://scaledagile.com'>SAFe</a>, <a href='https://Scrum.org'>Scrum.org</a>, and <a href='https://www.scrumalliance.org/'>Scrum Alliance.",
       buttonText: "view courses",
     },
-    {
-      name: "Coaching",
-      imageUrl: "AdobeStock_230418597",
-      text: "<ul><li>Maturity Assesment</li><li>Training</li><li>Coaching</li></ul>",
-      buttonText: "our offerings",
-    },
+    // {
+    //   name: "Coaching",
+    //   imageUrl: "AdobeStock_230418597",
+    //   text: "<ul><li>Maturity Assesment</li><li>Training</li><li>Coaching</li></ul>",
+    //   buttonText: "our offerings",
+    // },
   ];
 
   const SlideText = (props) => {
