bdaf671b10e8612c1d7420986badab17936da9cb
eduraTest <chaitraj@bu.edu>
2022-03-03 17:34:52 -0500

    Added all homepage tests

--- automation/src/test/java/stepDefinitions/HomepageStepDefinitions.java
+++ automation/src/test/java/stepDefinitions/HomepageStepDefinitions.java
@@ -140,4 +140,244 @@ public class HomepageStepDefinitions {
 		Assert.assertTrue("Left carousal button is not displayed", home.leftCarousalButton.isDisplayed());
 		Assert.assertTrue("Right carousal button is not displayed", home.rightCarousalButton.isDisplayed());
 	}
+	
+	@When("^I view Our Services section$")
+	public void i_view_Our_Services_section() throws Throwable{
+		Assert.assertTrue("Our services section is not displayed", home.ourServicesSection.isDisplayed());
+	}
+	
+	@Then("^I should see Our Services Title$")
+	public void i_should_see_Our_Services_Title() throws Throwable{
+		Assert.assertTrue("Our services title is not displayed", home.ourServicesTitle.isDisplayed());
+	}
+	
+	@Then("^I should see Assessment service and Courses Service images$")
+	public void i_should_see_Assessment_service_and_Courses_Service_images() throws Throwable{
+		Assert.assertTrue("Assessment image in our services is not displayed", home.ourServiceAssessmentImage.isDisplayed());
+		Assert.assertTrue("Courses image in our services is not displayed", home.ourServiceCoursesImage.isDisplayed());
+	}
+	
+	@Then("^I should see Assessment and Courses subtitles$")
+	public void i_should_see_Assessment_and_Courses_subtitles() throws Throwable{
+		Assert.assertTrue("Assessment subtitle in our services is not displayed", home.ourServicesAssessmentSubtitle.isDisplayed());
+		Assert.assertTrue("Courses subtitle in our services is not displayed", home.ourServicesCoursesSubtitle.isDisplayed());
+	}
+	
+	@Then("^I should see Assessment and courses description$")
+	public void i_should_see_Assessment_and_courses_description() throws Throwable{
+		Assert.assertTrue("Assessment description in our services is not displayed", home.ourServicesAssessmentDescription.isDisplayed());
+		Assert.assertTrue("Courses description in our services is not displayed", home.ourServicesCoursesDescription.isDisplayed());
+	}
+	
+	@Then("^I should see Assessment and courses learn more link$")
+	public void i_should_see_Assessment_and_courses_learn_more_link() throws Throwable{
+		Assert.assertTrue("Assessment learn more link in our services is not displayed", home.ourServicesAssessmentLearnMoreLink.isDisplayed());
+		Assert.assertTrue("Courses learn more link in our services is not displayed", home.ourServicesCoursesLearnMoreLink.isDisplayed());
+	}
+	
+	@When("^I click on assessment learn more link$")
+	public void i_click_on_assessment_learn_more_link() throws Throwable{
+		home.clickAssessmentLearnMoreLink();
+	}
+	
+	@When("^I click on courses learn more link")
+	public void i_click_on_courses_learn_more_link() throws Throwable{
+		home.clickCoursesLearnMoreLink();
+	}
+	
+	@When("^I view About Us section$")
+	public void i_view_About_Us_section() throws Throwable{
+		Assert.assertTrue("About us section is not displayed", home.aboutUsSection.isDisplayed());
+	}
+	
+	@Then("^I should see About Us title is displayed$")
+	public void i_should_see_About_Us_title_is_displayed() throws Throwable{
+		Assert.assertTrue("About us title is not displayed", home.aboutUsTitle.isDisplayed());
+	}
+
+	@Then("^I should see Who We are button is displayed$")
+	public void i_should_see_Who_We_are_button_is_displayed() throws Throwable{
+		Assert.assertTrue("Who we are button is not displayed", home.whoWeAreButton.isDisplayed());
+	}
+	
+	@Then("^I should see What we do button is displayed$")
+	public void i_should_see_What_we_do_button_is_displayed() throws Throwable{
+		Assert.assertTrue("What we do button is not displayed", home.whatWeDoButton.isDisplayed());
+	}
+	
+	@Then("^I should see Contact Us button is displayed$")
+	public void i_should_see_Contact_Us_button_is_displayed() throws Throwable{
+		Assert.assertTrue("Contact Us button is not displayed", home.contactUsButton.isDisplayed());
+	}
+	
+	@Then("^I should see description displayed to right of the buttons$")
+	public void i_should_see_description_displayed_to_right_of_the_button() throws Throwable{
+		Assert.assertTrue("Description is not displayed", home.aboutUsDescription.isDisplayed());
+	}
+	
+	@When("^I click on Who Are we button$")
+	public void i_click_on_Who_Are_we_button() throws Throwable{
+		home.clickWhatWeDoButton();
+	}
+	
+	@Then("^The description should show subtitle who are we$")
+	public void i_description_should_show_subtitle_who_are_we() throws Throwable{
+		Assert.assertTrue("Subtitle is not displayed", home.whoWeAreSubtitle.isDisplayed());
+	}
+	
+	@Then("^The details about who we are should be displayed$")
+	public void i_details_about_who_we_are_should_be_displayed() throws Throwable{
+		Assert.assertTrue("Who we are details is not displayed", home.whoWeAreDescription.isDisplayed());
+	}
+	
+	@When("^I click on What we do button$")
+	public void i_click_on_What_we_do_button() throws Throwable{
+		home.clickWhatWeDoButton();
+	}
+	
+	@Then("^The description should show subtitle what we do$")
+	public void The_description_should_show_subtitle_what_we_do() throws Throwable{
+		Assert.assertTrue("What we do subtitle is not displayed", home.whatWeDoSubtitle.isDisplayed());
+	}
+	
+	@Then("The details about what we do should be displayed$")
+	public void The_details_about_what_we_do_should_be_displayed() throws Throwable{
+		Assert.assertTrue("What we do description is not displayed", home.whatWeDoDescription.isDisplayed());
+	}
+	
+	@When("^I click on contact us button$")
+	public void i_click_on_contact_us_button() throws Throwable{
+		home.clickContactUsButton();
+	}
+	
+	@When("^I view Our Partners section$")
+	public void i_view_Our_Parnetrs_Section() throws Throwable{
+		Assert.assertTrue("Our Partners section is not displayed", home.ourPartnersSection.isDisplayed());
+	}
+	
+	@Then("^I should see Our Partner title displayed$")
+	public void i_should_see_Our_Partner_title_displayed() throws Throwable{
+		Assert.assertTrue("Our Partners title is not displayed", home.ourPartnersTitle.isDisplayed());
+	}
+	
+	@Then("^I should see scrum org logo displayed$")
+	public void i_should_see_scrum_org_logo_displayed() throws Throwable{
+		Assert.assertTrue("Scrum.org logo is not displayed", home.scrumOrgLogo.isDisplayed());
+	}
+	
+	@Then("^I should see scrum alliance logo displayed$")
+	public void i_should_see_scrum_alliance_logo_displayed() throws Throwable{
+		Assert.assertTrue("Scrum Alliance logo is not displayed", home.scrumAllianceLogo.isDisplayed());
+	}
+	
+	@Then("^I should see SAFE agile logo displayed$")
+	public void i_should_see_SAFE_agile_logo_displayed() throws Throwable{
+		Assert.assertTrue("Safe logo is not displayed", home.safeLogo.isDisplayed());
+	}
+	
+	@When("^I click on Scrum org logo$")
+	public void i_click_on_Scrum_org_logo() throws Throwable{
+		home.clickScrumOrgLogo();
+	}
+	
+	@Then("^I should be navigated to scrum org page$")
+	public void i_should_be_navigated_to_scrum_org_page() throws Throwable{
+		String Url = driver.getCurrentUrl();
+		Assert.assertTrue("User not navigated to Scrum org website", Url.contains("scrum.org"));
+	}
+	
+	@When("^I click on Scrum alliance logo$")
+	public void i_click_on_Scrum_alliance_logo() throws Throwable{
+		home.clickScrumAllianceLogo();
+	}
+	
+	@Then("^I should be navigated to scrum alliance page$")
+	public void i_should_be_navigated_to_scrum_alliance_page() throws Throwable{
+		String Url = driver.getCurrentUrl();
+		Assert.assertTrue("User not navigated to Scrum Alliance website", Url.contains("https://www.scrumalliance.org/"));
+	}
+	
+	@When("^I click on Safe agile logo$")
+	public void i_click_on_Safe_agile_logo() throws Throwable{
+		home.clickSafeLogo();
+	}
+	
+	@Then("^I should be navigated to safe agile page$")
+	public void i_should_be_navigated_to_safe_agile_page() throws Throwable{
+		String Url = driver.getCurrentUrl();
+		Assert.assertTrue("User not navigated to Safe website", Url.contains("https://www.scaledagileframework.com/"));
+	}
+	
+	@When("^I view get inspired section$")
+	public void i_view_get_inspired_section() throws Throwable{
+		Assert.assertTrue("Get inspired section is not displayed", home.getInspiredSection.isDisplayed());
+	}
+	
+	@Then("^I should see Get inspired title$")
+	public void i_should_see_Get_inspired_title() throws Throwable{
+		Assert.assertTrue("Get inspired title is not displayed", home.getInspiredTitle.isDisplayed());
+	}
+	
+	@Then("^I should see the reviews displayed$")
+	public void i_should_see_the_reviews_displayed() throws Throwable{
+		Assert.assertTrue("Get inspired reviews is not displayed", home.getInspiredReviews.isDisplayed());
+	}
+	
+	@Then("^I should see left and right carousal buttons$")
+	public void i_should_see_left_and_right_carousal_buttons() throws Throwable{
+		Assert.assertTrue("Get inspired left carousal is not displayed", home.getInspiredLeftSliderCarousal.isDisplayed());
+		Assert.assertTrue("Get inspired right carousal is not displayed", home.getInspiredRightSliderCarousal.isDisplayed());
+	}
+	
+	@When("^I view Stay Connected section$")
+	public void i_view_Stay_Connected_section() throws Throwable{
+		Assert.assertTrue("Stay Connected section is not displayed", home.stayConnectedSection.isDisplayed());
+	}
+	
+	@Then("^I should see Stay Connected title displayed$")
+	public void i_should_see_stay_connected_title_displayed() throws Throwable{
+		Assert.assertTrue("Stay Connected title is not displayed", home.stayConnectedTitle.isDisplayed());
+	}
+	
+	@Then("^I should see First name and Last name field displayed$")
+	public void i_should_see_First_name_and_Last_name_field_displayed() throws Throwable{
+		Assert.assertTrue("First name in Stay Connected is not displayed", home.firstNameField.isDisplayed());
+		Assert.assertTrue("Last name in Stay Connected is not displayed", home.lastNameField.isDisplayed());
+	}
+	
+	@Then("^I should see email Address field displayed$")
+	public void i_should_see_email_Address_field_displayed() throws Throwable{
+		Assert.assertTrue("Email Address in Stay Connected is not displayed", home.emailAddressField.isDisplayed());
+	}
+	
+	@Then("^I should see checkbox for I am permitting to contact me$")
+	public void i_should_see_checkbox_for_I_am_permitting_to_contact_me() throws Throwable{
+		Assert.assertTrue("Checkbox for I am permitting to contact me in Stay Connected is not displayed", home.checkBox.isDisplayed());
+	}
+	
+	@Then("^I should see Sign Up button$")
+	public void i_should_see_Sign_Up_button() throws Throwable{
+		Assert.assertTrue("Sign Up button in Stay Connected is not displayed", home.stayConnectedSignUpButton.isDisplayed());
+	}
+	
+	@When("^I view footer section$")
+	public void i_view_footer_section() throws Throwable{
+		Assert.assertTrue("Footer is not displayed", home.footerSection.isDisplayed());
+	}
+	
+	@Then("^I should see logo displayed$")
+	public void i_should_see_logo_displayed() throws Throwable{
+		Assert.assertTrue("Footer logo is not displayed", home.footerLogo.isDisplayed());
+	}
+	
+	@Then("^I should see Related links section displayed$")
+	public void i_should_see_Related_links_section_displayed() throws Throwable{
+		Assert.assertTrue("Related links section in footer is not displayed", home.relatedLinkSection.isDisplayed());
+	}
+	
+	@Then("^I should see Follow us links displayed with social media icons$")
+	public void i_should_see_Follow_us_links_displayed_with_social_media_icons() throws Throwable{
+		Assert.assertTrue("Follow us links with social media icons in footer is not displayed", home.followUsLinks.isDisplayed());
+	}
 }
+
